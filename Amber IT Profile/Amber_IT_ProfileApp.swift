e0304feac45dd6061d5c45a0d8a7e86e5381d76f
Md Abir Hossain <hossain.mdabir@gmail.com>
2023-09-27 11:39:15 +0600

    Initial Commit

---
+++ Amber IT Profile/Amber_IT_ProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Amber_IT_ProfileApp.swift
+//  Amber IT Profile
+//
+//  Created by Md Abir Hossain on 27/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Amber_IT_ProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
